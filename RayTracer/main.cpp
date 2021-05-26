5abef38ea481dc4b2f246e5c6452589a8723b2ec
wujinyuan.charles <wujinyuan.charles@bytedance.com>
2021-05-26 16:50:13 +0800

    finish Image Texture Mapping

--- RayTracer/main.cpp
+++ RayTracer/main.cpp
@@ -100,6 +100,13 @@ hittable_list two_perlin_spheres() {
 
     return objects;
 }
+hittable_list earth() {
+    auto earth_texture = make_shared<image_texture>("earthmap.jpg");
+    auto earth_surface = make_shared<lambertian>(earth_texture);
+    auto globe = make_shared<sphere>(point3(0,0,0), 2, earth_surface);
+
+    return hittable_list(globe);
+}
 color ray_color(const ray& r, const hittable& world,int depth) {
     hit_record rec;
     if (depth <= 0)
@@ -152,9 +159,15 @@ int main() {
             lookat = point3(0,0,0);
             vfov = 20.0;
             break;
-        default:
         case 3:
             world = two_perlin_spheres();
+            lookfrom = point3(13,2,3);
+            lookat = point3(0,0,0);
+            vfov = 20.0;
+            break;
+        default:
+        case 4:
+            world = earth();
             lookfrom = point3(13,2,3);
             lookat = point3(0,0,0);
             vfov = 20.0;
