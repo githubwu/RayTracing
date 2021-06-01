4192a36c38b995f9ec21a9f876f0be181272c3d1
wujinyuan.charles <wujinyuan.charles@bytedance.com>
2021-06-01 15:48:50 +0800

    Instances

--- RayTracer/main.cpp
+++ RayTracer/main.cpp
@@ -134,6 +134,19 @@ hittable_list cornell_box() {
     objects.add(make_shared<xz_rect>(0, 555, 0, 555, 0, white));
     objects.add(make_shared<xz_rect>(0, 555, 0, 555, 555, white));
     objects.add(make_shared<xy_rect>(0, 555, 0, 555, 555, white));
+    
+//    objects.add(make_shared<box>(point3(130, 0, 65), point3(295, 165, 230), white));
+//    objects.add(make_shared<box>(point3(265, 0, 295), point3(430, 330, 460), white));
+    
+    shared_ptr<hittable> box1 = make_shared<box>(point3(0, 0, 0), point3(165, 330, 165), white);
+    box1 = make_shared<rotate_y>(box1, 15);
+    box1 = make_shared<translate>(box1, vec3(265,0,295));
+    objects.add(box1);
+
+    shared_ptr<hittable> box2 = make_shared<box>(point3(0,0,0), point3(165,165,165), white);
+    box2 = make_shared<rotate_y>(box2, -18);
+    box2 = make_shared<translate>(box2, vec3(130,0,65));
+    objects.add(box2);
 
     return objects;
 }
@@ -221,7 +234,7 @@ int main() {
             world = cornell_box();
             aspect_ratio = 1.0;
             image_width = 600;
-            samples_per_pixel = 200;
+            samples_per_pixel = 100;
             background = color(0,0,0);
             lookfrom = point3(278, 278, -800);
             lookat = point3(278, 278, 0);
